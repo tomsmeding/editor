tomsmeding/editor

commit 9d6ee3568160e7d6bd4e58619fc0337a87710c90

Better w&b, more like vim's

io.cpp
@@ -7,6 +7,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <climits>
+#include <cctype>
 #include <unistd.h>
 #include <termios.h>
 #include <signal.h>
@@ -689,8 +690,16 @@ bool jumpToPreviousOccurrenceOfChar(Inter::Filebuffer &fbuf,char c,unsigned int 
 	return false;
 }
 
-bool isWordChar(char c,int llen){
-	return isalnum(c)||c=='_'||llen==0;
+enum CharCat{
+	CC_SPACE=0,
+	CC_ALNUM,
+	CC_PUNCT
+};
+
+CharCat charCategory(char c){
+	if(isspace(c))return CC_SPACE;
+	else if(isalnum(c))return CC_ALNUM;
+	else return CC_PUNCT;
 }
 
 int runloop(void){
@@ -770,30 +779,64 @@ int runloop(void){
 			Screen::redraw();
 			break;
 		}
-		case 'w': {
-			const unsigned int llen=fbuf.contents.linelen(fbuf.cury);
-			unsigned int i;
-			for(i=fbuf.curx;i<llen;i++){
-				const char curchar=fbuf.contents.at(i,fbuf.cury);
-				if(!isWordChar(curchar, llen)&&--repcount==0) {
-					fbuf.curx=i+1;
-					break;
+		case 'w':{
+			unsigned int x=fbuf.curx,y=fbuf.cury;
+			const unsigned int nln=fbuf.contents.numlines();
+			CharCat startcat=charCategory(fbuf.contents.at(x,y));
+			while(y<nln){
+				const unsigned int llen=fbuf.contents.linelen(y);
+				while(x<llen){
+					const char c=fbuf.contents.at(x,y);
+					const CharCat cat=charCategory(c);
+					cerr<<"x,y=("<<x<<','<<y<<") c="<<c<<" sc="<<startcat<<" cat="<<cat<<endl;
+					if(cat==CC_SPACE)startcat=CC_SPACE;
+					else if(cat!=startcat)break;
+					x++;
 				}
+				if(llen&&x<llen)break;
+				startcat=CC_SPACE; //for the newline
+				x=0;
+				y++;
 			}
-			fbuf.curx=fbuf.curx>=llen?llen-1:fbuf.curx;
+			if(nln&&y==nln){
+				y=nln-1;
+				const unsigned int llen=fbuf.contents.linelen(y);
+				x=llen==0?0:llen-1;
+			}
+			fbuf.curx=x; fbuf.cury=y;
 			Screen::redraw();
 			break;
 		}
-		case 'b': {
-			const unsigned int llen=fbuf.contents.linelen(fbuf.cury);
-			int i;
-			for(i=fbuf.curx;i>=0;i--){
-				const char curchar=fbuf.contents.at(i,fbuf.cury);
-				if(!isWordChar(curchar, llen)&&--repcount==0) {
-					fbuf.curx=i<=1?0:(i-1);
-					break;
+		case 'b':{
+			int x=fbuf.curx,y=fbuf.cury;
+			CharCat startcat=charCategory(fbuf.contents.at(x,y));
+			while(y>=0){
+				while(x>=0){
+					const char c=fbuf.contents.at(x,y);
+					const CharCat cat=charCategory(c);
+					if(cat==CC_SPACE)startcat=CC_SPACE;
+					else if(cat!=startcat)break;
+					x--;
 				}
+				if(x>=0)break;
+				startcat=CC_SPACE; //for the newline
+				y--;
+				if(y>=0)x=fbuf.contents.linelen(y)-1;
 			}
+			if(y<0){
+				x=0; y=0;
+			} else {
+				startcat=charCategory(fbuf.contents.at(x,y));
+				x--;
+				while(x>=0){
+					const char c=fbuf.contents.at(x,y);
+					const CharCat cat=charCategory(c);
+					if(cat!=startcat)break;
+					x--;
+				}
+				x++;
+			}
+			fbuf.curx=x; fbuf.cury=y;
 			Screen::redraw();
 			break;
 		}

commit 43bf2b2e83ad52ee341a5b80b660f3d1255b310c

count _ as a word char
Vim does it.
This was broken by 9d6ee3568160e7d6bd4e58619fc0337a87710c90

io.cpp
@@ -697,13 +697,13 @@ bool jumpToPreviousOccurrenceOfChar(Inter::Filebuffer &fbuf,char c,unsigned int 
 
 enum CharCat{
 	CC_SPACE=0,
-	CC_ALNUM,
+	CC_WORD,
 	CC_PUNCT
 };
 
 CharCat charCategory(char c){
 	if(isspace(c))return CC_SPACE;
-	else if(isalnum(c))return CC_ALNUM;
+	else if(isalnum(c)||c=='_')return CC_WORD;
 	else return CC_PUNCT;
 }
 